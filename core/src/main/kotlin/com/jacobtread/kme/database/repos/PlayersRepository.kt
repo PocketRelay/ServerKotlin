ac248f6a69b2890ddef79a6ecbbb55314d2f2abb
Jacobtread <jacobtread@gmail.com>
2022-05-14 11:54:29 +1200

    Cleaned up abstraction and documented players repository

--- core/src/main/kotlin/com/jacobtread/kme/database/repos/PlayersRepository.kt
+++ core/src/main/kotlin/com/jacobtread/kme/database/repos/PlayersRepository.kt
@@ -8,50 +8,130 @@ import java.sql.SQLException
 
 class PlayerCreationException(reason: String, cause: Throwable? = null) : Exception(reason, cause)
 class PlayerNotFoundException : Exception()
-class ServerErrorException : Exception()
+class ServerErrorException(cause: Throwable? = null) : Exception(cause)
 
+/**
+ * PlayersRepository Represents a repository that is used to create, store,
+ * search and update player/user data
+ *
+ * @constructor Create empty PlayersRepository
+ */
 abstract class PlayersRepository : DatabaseRepository() {
 
+    /**
+     * isDisplayNameTaken Database implementation for checking whether the provided
+     * display name is already taken by another user account
+     *
+     * @param name The display name to check for
+     * @return Whether the name is taken
+     */
     abstract fun isDisplayNameTaken(name: String): Boolean
+
+    /**
+     * isEmailTaken Database implementation for checking whether the provided
+     * email is already taken by another user account
+     *
+     * @param email The email to check for
+     * @return Whether the email is taken
+     */
     abstract fun isEmailTaken(email: String): Boolean
-    abstract fun createPlayerInternal(email: String, displayName: String, hashedPassword: String)
+
+    /**
+     * createPlayerInternal Database implementation of creating player wrapped by
+     * createPlayer and shouldn't be called directly.
+     *
+     * @param email The email of the player account
+     * @param displayName The display name of the player (username)
+     * @param hashedPassword The hashed password
+     */
+    protected abstract fun createPlayerInternal(email: String, displayName: String, hashedPassword: String)
+
+    /**
+     * getPlayerByName Retrieves a player from the repository that has a matching
+     * name to the provided display name
+     *
+     * @param displayName The name of the player to search for
+     * @return The player that was found
+     * @throws ServerErrorException Thrown when an exception was occurred between the server and database
+     * @throws PlayerNotFoundException Thrown if the player doesn't exist
+     */
+    @Throws(ServerErrorException::class, PlayerNotFoundException::class)
     abstract fun getPlayerByName(displayName: String): Player
 
-    fun createPlayer(email: String, displayName: String, password: String): Boolean {
+    /**
+     * getPlayerByNameOrNull Uses getPlayerByName for underlying implementation
+     * but returns null if any exceptions are caught
+     *
+     * @param displayName The name of the player to search for
+     * @return The player that was found or null
+     */
+    fun getPlayerByNameOrNull(displayName: String): Player? {
+        return try {
+            getPlayerByName(displayName)
+        } catch (_: Exception) {
+            return null
+        }
+    }
+
+    /**
+     * createPlayer Creates a new player using the underlying repository implementations
+     * along with hashing the password and ensuring the account is unique and doesn't
+     * already exist.
+     *
+     * @param email The email for this account
+     * @param displayName The display name for this account
+     * @param password The plain text password to use for this account (This will be hashed)
+     * @throws PlayerCreationException Thrown if creation of the player fails
+     */
+    @Throws(PlayerCreationException::class)
+    fun createPlayer(email: String, displayName: String, password: String) {
         try {
             if (isEmailTaken(email)) throw PlayerCreationException("That email is already in use.")
             if (isDisplayNameTaken(displayName)) throw PlayerCreationException("That display name is already in use.")
             val hashed = hashPassword(password)
             createPlayerInternal(email, displayName, hashed)
-            return true
         } catch (e: Exception) {
             LOGGER.error("Failed to create player", e)
             throw PlayerCreationException("Failed to create player", e)
         }
     }
 
-    class MySQL(private val connection: Connection) : PlayersRepository() {
+    /**
+     * MySQL Defines the behavior for the players' repository behavior when using
+     * a MySQL database. The SQLite database inherits its behavior from this
+     *
+     * @property connection
+     * @constructor Create empty MySQL
+     */
+    open class MySQL(private val connection: Connection) : PlayersRepository() {
+
+        /**
+         * initQuery Abstraction of the query used to create the players'
+         * database table. This is used to make to easier for the SQLite
+         * adapter to use a different create query
+         *
+         * @return The string SQL query
+         */
+        open fun initQuery(): String = """
+                CREATE TABLE IF NOT EXISTS `players` (
+                    `id` INT(255) AUTO_INCREMENT,
+                    `email` VARCHAR(254),
+                    `display_name` VARCHAR(99),
+                    `credits` INT(10) UNSIGNED,
+                    `games_played` INT(10) UNSIGNED DEFAULT 0,
+                    `auth` VARCHAR(128) DEFAULT NULL,
+                    `auth2` VARCHAR(128) DEFAULT NULL,
+                    UNIQUE KEY `id_index` (`id`) USING BTREE,
+                    UNIQUE KEY `name_index` (`display_name`) USING BTREE,
+                    PRIMARY KEY (`id`)
+                );
+            """.trimIndent()
+
         override fun init() {
             try {
                 val statement = connection.createStatement()
-                statement.execute(
-                    """
-                    CREATE TABLE IF NOT EXISTS `players` (
-                        `id` INT(255) AUTO_INCREMENT,
-                        `email` VARCHAR(254),
-                        `display_name` VARCHAR(99),
-                        `credits` INT(10) unsigned,
-                        `games_played` INT(10) unsigned DEFAULT 0,
-                        `auth` VARCHAR(128) DEFAULT NULL,
-                        `auth2` VARCHAR(128) DEFAULT NULL,
-                        UNIQUE KEY `id_index` (`id`) USING BTREE,
-                        UNIQUE KEY `name_index` (`display_name`) USING BTREE,
-                        PRIMARY KEY (`id`)
-                    );
-                    """.trimIndent()
-                )
+                statement.execute(initQuery())
                 LOGGER.info("Created players database table")
-                LOGGER.info("Creating example player")
             } catch (e: SQLException) {
                 LOGGER.fatal("Failed to create players database table", e)
             }
@@ -96,30 +176,31 @@ abstract class PlayersRepository : DatabaseRepository() {
                     result.getString("auth2"),
                 )
             } catch (e: SQLException) {
-                throw ServerErrorException()
+                throw ServerErrorException(e)
             }
         }
     }
 
-    class SQLite(private val connection: Connection): PlayersRepository() {
-        override fun init() {
-            TODO("Not yet implemented")
-        }
-
-        override fun isDisplayNameTaken(name: String): Boolean {
-            TODO("Not yet implemented")
-        }
-
-        override fun isEmailTaken(email: String): Boolean {
-            TODO("Not yet implemented")
-        }
-
-        override fun createPlayerInternal(email: String, displayName: String, hashedPassword: String) {
-            TODO("Not yet implemented")
-        }
-
-        override fun getPlayerByName(displayName: String): Player {
-            TODO("Not yet implemented")
-        }
+    /**
+     * SQLite Defines the SQLite behavior for the players' repository. This
+     * inherits all other query behavior from the MySQL repository excluding
+     * the initialization query
+     *
+     * @property connection
+     * @constructor Create empty SQLite
+     */
+    class SQLite(private val connection: Connection) : MySQL(connection) {
+        override fun initQuery(): String =
+            """
+            CREATE TABLE IF NOT EXISTS `players` (
+                `id` INT AUTO_INCREMENT PRIMARY KEY UNIQUE ,
+                `email` TEXT UNIQUE ,
+                `display_name` TEXT UNIQUE,
+                `credits` INT,
+                `games_played` INT DEFAULT 0,
+                `auth` TEXT DEFAULT NULL,
+                `auth2` TEXT DEFAULT NULL
+            );
+            """.trimIndent()
     }
 }
