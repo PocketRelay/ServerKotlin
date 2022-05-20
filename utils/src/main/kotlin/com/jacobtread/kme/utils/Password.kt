a48d7e30c5969a34adf960c0c7e6bd5a86999e0a
Jacobtread <jacobtread@gmail.com>
2022-05-20 12:25:17 +1200

    Properly implemented password hashing

--- utils/src/main/kotlin/com/jacobtread/kme/utils/Password.kt
+++ utils/src/main/kotlin/com/jacobtread/kme/utils/Password.kt
@@ -2,17 +2,23 @@ package com.jacobtread.kme.utils
 
 import java.security.SecureRandom
 import java.util.*
+import java.util.regex.Pattern
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
 // The cost of the hashing process 1 < HASH_COST = total iterations
 private const val HASH_COST = 16
+
 // The length of the hash key
 private const val HASH_SIZE = 128
+
 // The hashing algorithm to use
 private const val HASH_ALGORITHM = "PBKDF2WithHmacSHA512"
+
 // The prefix to append to the hashed password
 private const val HASH_PREFIX = "$31$"
+
+private val HASH_PATTERN = Pattern.compile("\\$31\\$(\\d\\d?)\\$(.{43})")
 
 /**
  * hashPassword Hashes the password using the provided
@@ -25,13 +31,18 @@ fun hashPassword(password: String): String {
     val random = SecureRandom()
     val salt = ByteArray(16)
     random.nextBytes(salt)
-    val spec = PBEKeySpec(password.toCharArray(), salt, 1 shl HASH_COST, HASH_SIZE)
-    val keyFactory = SecretKeyFactory.getInstance(HASH_ALGORITHM)
-    val hash = keyFactory.generateSecret(spec).encoded
+    val hash = hashValue(password.toCharArray(), salt, HASH_COST)
+    val hashSalt = salt + hash
     val encoded = Base64.getUrlEncoder()
         .withoutPadding()
-        .encodeToString(hash);
+        .encodeToString(hashSalt);
     return "$HASH_PREFIX$HASH_COST\$$encoded"
+}
+
+private fun hashValue(password: CharArray, salt: ByteArray, cost: Int): ByteArray {
+    val spec = PBEKeySpec(password, salt, 1 shl cost, HASH_SIZE)
+    val keyFactory = SecretKeyFactory.getInstance(HASH_ALGORITHM)
+    return keyFactory.generateSecret(spec).encoded
 }
 
 /**
@@ -43,5 +54,12 @@ fun hashPassword(password: String): String {
  * @return Whether the passwords match
  */
 fun comparePasswordHash(password: String, hashed: String): Boolean {
-    return hashPassword(password) == hashed
+    val hashMatcher = HASH_PATTERN.matcher(hashed)
+    if (!hashMatcher.matches()) return false
+    val cost = hashMatcher.group(1).toIntOrNull() ?: HASH_COST
+    val contents = Base64.getUrlDecoder().decode(hashMatcher.group(2))
+    val salt = contents.copyOfRange(0, 16)
+    val hash = contents.copyOfRange(16, contents.size)
+    val newHash = hashValue(password.toCharArray(), salt, cost)
+    return hash.contentEquals(newHash)
 }
