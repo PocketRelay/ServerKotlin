f9d819c905e837a78b8510bd914d0ed3e26714e5
Jacobtread <jacobtread@gmail.com>
2022-05-25 10:16:31 +1200

    Improved tdf helpers

--- blaze/src/main/kotlin/com.jacobtread.kme.blaze/TdfContainer.kt
+++ blaze/src/main/kotlin/com.jacobtread.kme.blaze/TdfContainer.kt
@@ -23,14 +23,14 @@ interface TdfContainer {
     }
 
     @Throws(InvalidTdfException::class)
-    fun <T, C : Tdf<T>> get(type: Class<C>, label: String): T {
+    fun <T, C : Tdf<T>> getValue(type: Class<C>, label: String): T {
         val value = getTdfByLabel(label) ?: throw MissingTdfException(label)
         if (!value.javaClass.isAssignableFrom(type)) throw InvalidTdfException(label, type, value.javaClass)
         return type.cast(value).value
 
     }
 
-    fun <T, C : Tdf<T>> getOrNull(type: Class<C>, label: String): T? {
+    fun <T, C : Tdf<T>> getValueOrNull(type: Class<C>, label: String): T? {
         val value = getTdfByLabel(label)
         if (value == null || !value.javaClass.isAssignableFrom(type)) return null
         return type.cast(value).value
@@ -59,21 +59,22 @@ inline fun TdfContainer.tripple(label: String): VarTripple = getTdf(TrippleTdf::
 inline fun TdfContainer.pair(label: String): VarPair = getTdf(PairTdf::class.java, label).value
 inline fun TdfContainer.varIntList(label: String): List<Long> = getTdf(VarIntList::class.java, label).value
 inline fun TdfContainer.list(label: String): List<Any> = getTdf(ListTdf::class.java, label).value
+
 @Suppress("UNCHECKED_CAST")
-inline fun <K: Any, V: Any> TdfContainer.map(label: String): Map<K, V> = getTdf(MapTdf::class.java, label).value as Map<K, V>
+inline fun <K : Any, V : Any> TdfContainer.map(label: String): Map<K, V> = getTdf(MapTdf::class.java, label).value as Map<K, V>
 
 // Nullable Helpers
 
-inline fun TdfContainer.textOrNull(label: String): String? = getTdfOrNull(StringTdf::class.java, label)?.value
-inline fun TdfContainer.numberOrNull(label: String): Long? = getTdfOrNull(VarIntTdf::class.java, label)?.value
+inline fun TdfContainer.textOrNull(label: String): String? = getValueOrNull(StringTdf::class.java, label)
+inline fun TdfContainer.numberOrNull(label: String): Long? = getValueOrNull(VarIntTdf::class.java, label)
 inline fun TdfContainer.numberIntOrNull(label: String): Int? = numberOrNull(label)?.toInt()
-inline fun TdfContainer.floatOrNull(label: String): Float? = getTdfOrNull(FloatTdf::class.java, label)?.value
-inline fun TdfContainer.blobOrNull(label: String): ByteArray? = getTdfOrNull(BlobTdf::class.java, label)?.value
-inline fun TdfContainer.unionValueOrNull(label: String): Tdf<*>? = getTdfOrNull(OptionalTdf::class.java, label)?.value
-inline fun TdfContainer.trippleOrNull(label: String): VarTripple? = getTdfOrNull(TrippleTdf::class.java, label)?.value
-inline fun TdfContainer.pairOrNull(label: String): VarPair? = getTdfOrNull(PairTdf::class.java, label)?.value
-inline fun TdfContainer.varIntListOrNull(label: String): List<Long>? = getTdfOrNull(VarIntList::class.java, label)?.value
-inline fun TdfContainer.listOrNull(label: String): List<Any>? = getTdfOrNull(ListTdf::class.java, label)?.value
+inline fun TdfContainer.floatOrNull(label: String): Float? = getValueOrNull(FloatTdf::class.java, label)
+inline fun TdfContainer.blobOrNull(label: String): ByteArray? = getValueOrNull(BlobTdf::class.java, label)
+inline fun TdfContainer.unionValueOrNull(label: String): Tdf<*>? = getValueOrNull(OptionalTdf::class.java, label)
+inline fun TdfContainer.trippleOrNull(label: String): VarTripple? = getValueOrNull(TrippleTdf::class.java, label)
+inline fun TdfContainer.pairOrNull(label: String): VarPair? = getValueOrNull(PairTdf::class.java, label)
+inline fun TdfContainer.varIntListOrNull(label: String): List<Long>? = getValueOrNull(VarIntList::class.java, label)
+inline fun TdfContainer.listOrNull(label: String): List<Any>? = getValueOrNull(ListTdf::class.java, label)
 
 @Suppress("UNCHECKED_CAST")
 inline fun <K : Any, V : Any> TdfContainer.mapOrNull(label: String): Map<K, V>? = getTdfOrNull(MapTdf::class.java, label)?.value as Map<K, V>?
