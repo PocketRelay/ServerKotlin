977deca9e8754d7e0d8cbfdaa476eebd0b738235
Jacobtread <jacobtread@gmail.com>
2022-05-24 20:20:02 +1200

    Fixed and cleaned up tag writing

--- blaze/src/main/kotlin/com/jacobtread/kme/blaze/tdf/Tdf.kt
+++ blaze/src/main/kotlin/com/jacobtread/kme/blaze/tdf/Tdf.kt
@@ -1,6 +1,5 @@
 package com.jacobtread.kme.blaze.tdf
 
-import com.jacobtread.kme.blaze.utils.Labels
 import com.jacobtread.kme.utils.VarTripple
 import io.netty.buffer.ByteBuf
 
@@ -33,10 +32,69 @@ abstract class Tdf<V>(val label: String, private val tagType: Int) {
             }
         }
 
+        fun createTag(labelIn: String): Long {
+            val res = IntArray(3)
+            val buff = IntArray(4)
+            for (i in 0 until 4) {
+                if (i >= labelIn.length) break
+                buff[i] = labelIn[i].code
+            }
+            res[0] = res[0].or(buff[0].and(0x40).shl(1))
+            res[0] = res[0].or(buff[0].and(0x10).shl(2))
+            res[0] = res[0].or(buff[0].and(0x0F).shl(2))
+            res[0] = res[0].or(buff[1].and(0x40).shr(5))
+            res[0] = res[0].or(buff[1].and(0x10).shr(4))
+
+            res[1] = res[1].or(buff[1].and(0x0F).shl(4))
+            res[1] = res[1].or(buff[2].and(0x40).shr(3))
+            res[1] = res[1].or(buff[2].and(0x10).shr(2))
+            res[1] = res[1].or(buff[2].and(0x0C).shr(2))
+
+            res[2] = res[2].or(buff[2].and(0x03).shl(6))
+            res[2] = res[2].or(buff[3].and(0x40).shr(1))
+            res[2] = res[2].or(buff[3].and(0x1F))
+
+            var tag = 0L
+            tag = tag.or(res[0].shl(24).toLong())
+            tag = tag.or(res[1].shl(16).toLong())
+            tag = tag.or(res[2].shl(8).toLong())
+            return tag
+        }
+
+        fun createLabel(tag: Long): String {
+            val buff = byteArrayOf(
+                (tag shr 24).toByte(),
+                (tag shr 16).toByte(),
+                (tag shr 8).toByte(),
+                tag.toByte()
+            )
+            val res = IntArray(4)
+            res[0] = res[0] or (buff[0].toInt() and 0x80 shr 1)
+            res[0] = res[0] or (buff[0].toInt() and 0x40 shr 2)
+            res[0] = res[0] or (buff[0].toInt() and 0x30 shr 2)
+            res[0] = res[0] or (buff[0].toInt() and 0x0C shr 2)
+
+            res[1] = res[1] or (buff[0].toInt() and 0x02 shl 5)
+            res[1] = res[1] or (buff[0].toInt() and 0x01 shl 4)
+            res[1] = res[1] or (buff[1].toInt() and 0xF0 shr 4)
+
+            res[2] = res[2] or (buff[1].toInt() and 0x08 shl 3)
+            res[2] = res[2] or (buff[1].toInt() and 0x04 shl 2)
+            res[2] = res[2] or (buff[1].toInt() and 0x03 shl 2)
+            res[2] = res[2] or (buff[2].toInt() and 0xC0 shr 6)
+            res[3] = res[3] or (buff[2].toInt() and 0x20 shl 1)
+            res[3] = res[3] or (buff[2].toInt() and 0x1F)
+            val output = StringBuilder()
+            for (i in 0..3) {
+                if (res[i] != 0) output.append(res[i].toChar())
+            }
+            return output.toString()
+        }
+
         fun read(input: ByteBuf): Tdf<*> {
             val head = input.readUnsignedInt()
             val tag = (head and 0xFFFFFF00)
-            val label = Labels.fromTag(tag)
+            val label = createLabel(tag)
             return when (val type = (head and 0xFF).toInt()) {
                 VARINT -> VarIntTdf.read(label, input)
                 STRING -> StringTdf.read(label, input)
@@ -59,7 +117,7 @@ abstract class Tdf<V>(val label: String, private val tagType: Int) {
     abstract fun write(out: ByteBuf)
 
     fun writeFully(out: ByteBuf) {
-        val tag = Labels.toTag(label)
+        val tag = createTag(label)
         out.writeByte(tag.shr(24).and(0xFF).toInt())
         out.writeByte(tag.shr(16).and(0xFF).toInt())
         out.writeByte(tag.shr(8).and(0xFF).toInt())
---
+++ blaze/src/test/kotlin/TagTest.kt
@@ -0,0 +1,8 @@
+import com.jacobtread.kme.blaze.tdf.Tdf
+
+fun main() {
+    val value = Tdf.createTag("TEST")
+
+    println(Tdf.createLabel(value))
+
+}
