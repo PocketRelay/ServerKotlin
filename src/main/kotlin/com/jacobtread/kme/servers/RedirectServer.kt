f4218c202d4ef95b5ae0fcae403ee9ef9e796645
Jacobtread <jacobtread@gmail.com>
2022-05-28 18:47:15 +1200

    Improved redirector server

--- src/main/kotlin/com/jacobtread/kme/servers/RedirectServer.kt
+++ src/main/kotlin/com/jacobtread/kme/servers/RedirectServer.kt
@@ -4,7 +4,6 @@ package com.jacobtread.kme.servers
 
 import com.jacobtread.kme.Config
 import com.jacobtread.kme.blaze.*
-import com.jacobtread.kme.utils.ServerAddress
 import com.jacobtread.kme.utils.logging.Logger
 import com.jacobtread.kme.utils.logging.Logger.error
 import com.jacobtread.kme.utils.logging.Logger.info
@@ -14,12 +13,13 @@ import io.netty.channel.Channel
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInitializer
-import io.netty.channel.SimpleChannelInboundHandler
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.handler.ssl.SslContext
 import io.netty.handler.ssl.SslContextBuilder
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory
+import io.netty.util.concurrent.Future
+import io.netty.util.concurrent.FutureListener
 import java.io.IOException
 import java.net.UnknownHostException
 import java.security.KeyStore
@@ -30,21 +30,13 @@ import javax.net.ssl.KeyManagerFactory
 fun startRedirector(bossGroup: NioEventLoopGroup, workerGroup: NioEventLoopGroup, config: Config) {
     try {
         val listenPort = config.ports.redirector
-        val targetAddress = lookupServerAddress(config.externalAddress)
-        val targetPort = config.ports.main
+        val initializer = RedirectorHandler(config)
         ServerBootstrap()
             .group(bossGroup, workerGroup)
             .channel(NioServerSocketChannel::class.java)
-            .childHandler(RedirectorInitializer(targetAddress, targetPort))
-            // Bind the server to the host and port
+            .childHandler(initializer)
             .bind(listenPort)
-            // Wait for the channel to bind
-            .addListener {
-                info("Started Redirector on port $listenPort redirecting to:")
-                info("Host: ${targetAddress.host}")
-                info("IP: ${targetAddress.ip}")
-                info("Port: $targetPort")
-            }
+            .addListener(initializer)
     } catch (e: UnknownHostException) {
         Logger.fatal("Unable to lookup server address \"${config.externalAddress}\"", e)
     } catch (e: IOException) {
@@ -53,28 +45,33 @@ fun startRedirector(bossGroup: NioEventLoopGroup, workerGroup: NioEventLoopGroup
 }
 
 /**
- * RedirectorInitializer Sets up channels for the redirector server. Creates
- * a SSLv3 context and applies that along with packet decoding, encoding, and
- *
+ * RedirectorHandler Handles all logic for channels that are created
+ * for the redirector server. This includes SSLv3 decoding/encoding,
+ * packet encoding/decoding as well as handling of decoded packets
  *
  * @constructor
  *
- * @param targetAddress The target address that should be redirected to
- * @param targetPort The target port that should be redirected to
+ * @param config The server configuration
  */
-class RedirectorInitializer(targetAddress: ServerAddress, targetPort: Int) : ChannelInitializer<Channel>() {
+@Sharable
+class RedirectorHandler(private val config: Config) : ChannelInitializer<Channel>(), FutureListener<Void> {
 
+    private val targetAddress = lookupServerAddress(config.externalAddress)
+    private val targetPort = config.ports.main
     private val context = createSslContext()
-    private val handler = RedirectHandler(targetAddress, targetPort)
 
+    /**
+     * initChannel Handles channel initialization this adds the
+     * encoders and decoders for both SSL and packets
+     *
+     * @param ch The channel to initialize
+     */
     override fun initChannel(ch: Channel) {
         ch.pipeline()
             // Add handler for decoding SSLv3
             .addLast(context.newHandler(ch.alloc()))
             // Add handler for decoding packets
             .addLast(PacketDecoder())
-            // Add handler for processing packets
-            .addLast(handler)
             // Add handler for encoding packets
             .addLast(PacketEncoder())
     }
@@ -89,7 +86,7 @@ class RedirectorInitializer(targetAddress: ServerAddress, targetPort: Int) : Cha
         // Clears the disabled algorithms necessary for SSLv3
         Security.setProperty("jdk.tls.disabledAlgorithms", "")
         val keyStorePassword = charArrayOf('1', '2', '3', '4', '5', '6')
-        val keyStoreStream = RedirectHandler::class.java.getResourceAsStream("/redirector.pfx")
+        val keyStoreStream = RedirectorHandler::class.java.getResourceAsStream("/redirector.pfx")
             ?: throw IllegalStateException("Missing required keystore for SSLv3")
         val keyStore = KeyStore.getInstance("PKCS12")
         keyStore.load(keyStoreStream, keyStorePassword)
@@ -104,44 +101,47 @@ class RedirectorInitializer(targetAddress: ServerAddress, targetPort: Int) : Cha
             .trustManager(InsecureTrustManagerFactory.INSTANCE)
             .build() ?: throw IllegalStateException("Unable to create SSL Context")
     }
-}
 
+    /**
+     * operationComplete For listening to the future of the
+     * redirector server bind completion
+     *
+     * @param future Ignored
+     */
+    override fun operationComplete(future: Future<Void>) {
+        val listenPort = config.ports.redirector
+        info("Started Redirector on port $listenPort redirecting to:")
+        info("Host: ${targetAddress.host}")
+        info("IP: ${targetAddress.ip}")
+        info("Port: $targetPort")
+    }
 
-/**
- * RedirectHandler a handler shared by all connections
- * that connect to the redirect server. Ignores all packets that aren't
- * REDIRECTOR + GET_SERVER_INSTANCE and when it gets them it sends the
- * client a redirect
- *
- * @constructor Create empty RedirectHandler
- */
-@Sharable
-class RedirectHandler(
-    private val target: ServerAddress,
-    private val port: Int,
-) : SimpleChannelInboundHandler<Packet>() {
-
-    override fun channelRead0(ctx: ChannelHandlerContext, msg: Packet) {
+    /**
+     * channelRead Handles interacting with the result of a read if the
+     * read value is a packet then it is handled and if it's a GET_SERVER_INSTANCE
+     * REDIRECTOR packet it is responded to with the redirect details
+     *
+     * @param ctx The channel context
+     * @param msg The message that was read
+     */
+    override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+        if (msg !is Packet) return
         val channel = ctx.channel()
-        if (msg.component == Components.REDIRECTOR
-            && msg.command == Commands.GET_SERVER_INSTANCE
-        ) {
-            val remoteAddress = channel.remoteAddress()
-            channel.respond(msg) {
+        channel.respond(msg) {
+            if (msg.component == Components.REDIRECTOR && msg.command == Commands.GET_SERVER_INSTANCE) {
                 optional("ADDR", group("VALU") {
-                    if (target.isHostname) {
-                        text("HOST", target.host)
+                    if (targetAddress.isHostname) {
+                        text("HOST", targetAddress.host)
                     }
-                    number("IP", target.address)
-                    number("PORT", port)
+                    number("IP", targetAddress.address)
+                    number("PORT", targetPort)
                 })
                 bool("SECU", false)
                 bool("XDNS", false)
+                val remoteAddress = channel.remoteAddress()
+                info("Sent redirection to client at $remoteAddress. Closing Connection.")
             }
-            info("Sent redirection to client at $remoteAddress. Closing Connection.")
-            channel.close()
-        } else {
-            channel.respond(msg)
         }
+        channel.close()
     }
 }
