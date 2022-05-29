717c49d11a8e69d16f75b878786195396923f69b
Jacobtread <jacobtread@gmail.com>
2022-05-29 12:36:30 +1200

    Cleaned up http server initializer

--- src/main/kotlin/com/jacobtread/kme/servers/http/HttpServer.kt
+++ src/main/kotlin/com/jacobtread/kme/servers/http/HttpServer.kt
@@ -3,7 +3,6 @@ package com.jacobtread.kme.servers.http
 import com.jacobtread.kme.Config
 import com.jacobtread.kme.servers.http.controllers.APIController
 import com.jacobtread.kme.servers.http.controllers.GAWController
-import com.jacobtread.kme.servers.http.router.Router
 import com.jacobtread.kme.servers.http.router.groupedRoute
 import com.jacobtread.kme.servers.http.router.router
 import com.jacobtread.kme.utils.logging.Logger
@@ -16,6 +15,8 @@ import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.handler.codec.http.HttpObjectAggregator
 import io.netty.handler.codec.http.HttpRequestDecoder
 import io.netty.handler.codec.http.HttpResponseEncoder
+import io.netty.util.concurrent.Future
+import io.netty.util.concurrent.FutureListener
 import java.io.IOException
 
 /**
@@ -27,34 +28,13 @@ import java.io.IOException
  */
 fun startHttpServer(bossGroup: NioEventLoopGroup, workerGroup: NioEventLoopGroup, config: Config) {
     try {
-        val port = config.ports.http
-        val router = router(config) {
-            +GAWController
-            if (config.panel.enabled) {
-                +groupedRoute("panel") {
-                    +APIController
-                    get(":*") { _, request ->
-                        val path = request.param("*")
-                        request.static(path, "panel", "index.html", "panel")
-                    }
-                }
-            }
-            // Contents catchall for the assets ME3 fetches
-            get("content/:*") { _, request ->
-                val path = request.param("*")
-                val fileName = path.substringAfterLast('/')
-                request.setHeader("Accept-Ranges", "bytes")
-                request.setHeader("ETag", "524416-1333666807000")
-                request.static(fileName, "public")
-            }
-        }
-        val initializer = HttpInitializer(router)
+        val initializer = HttpInitializer(config)
         ServerBootstrap()
             .group(bossGroup, workerGroup)
             .channel(NioServerSocketChannel::class.java)
             .childHandler(initializer)
-            .bind(port)
-            .addListener { Logger.info("Started HTTP Server on port $port") }
+            .bind(config.ports.http)
+            .addListener(initializer)
     } catch (e: IOException) {
         Logger.error("Exception in HTTP server", e)
     }
@@ -68,12 +48,53 @@ fun startHttpServer(bossGroup: NioEventLoopGroup, workerGroup: NioEventLoopGroup
  * @constructor Create empty HttpInitializer
  */
 @Sharable
-class HttpInitializer(private val router: Router) : ChannelInitializer<Channel>() {
+class HttpInitializer(private val config: Config) : ChannelInitializer<Channel>(), FutureListener<Void> {
+
+    /**
+     * router Initializing the router paths
+     */
+    val router = router(config) {
+        +GAWController // Galaxy at war routing group
+        if (config.panel.enabled) { // If the panel is enabled
+            +groupedRoute("panel") { // Panel routing group
+                +APIController // API routing group
+                get(":*") { _, request -> // Catchall for static assets falling back on index.html
+                    val path = request.param("*")
+                    request.static(path, "panel", "index.html", "panel")
+                }
+            }
+        }
+        // Contents catchall for the assets ME3 fetches
+        get("content/:*") { _, request ->
+            val path = request.param("*")
+            val fileName = path.substringAfterLast('/')
+            request.setHeader("Accept-Ranges", "bytes")
+            request.setHeader("ETag", "524416-1333666807000")
+            request.static(fileName, "public")
+        }
+    }
+
+    /**
+     * initChannel Initializes the channel adding the http encoders,
+     * decoders and content aggregator and the router
+     *
+     * @param ch
+     */
     override fun initChannel(ch: Channel) {
         ch.pipeline()
             .addLast(HttpRequestDecoder())
             .addLast(HttpResponseEncoder())
             .addLast(HttpObjectAggregator(1024 * 8))
             .addLast(router)
+    }
+
+    /**
+     * operationComplete For listening to the future of the
+     * http server bind completion
+     *
+     * @param future Ignored
+     */
+    override fun operationComplete(future: Future<Void>) {
+        Logger.info("Started HTTP Server on port ${config.ports.http}")
     }
 }
