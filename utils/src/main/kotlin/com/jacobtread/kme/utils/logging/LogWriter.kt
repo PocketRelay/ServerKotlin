58ed84332aca43db49997e1dc0b974d5a44c7830
Jacobtread <jacobtread@gmail.com>
2022-06-04 17:31:53 +1200

    Improved log writer

--- utils/src/main/kotlin/com/jacobtread/kme/utils/logging/LogWriter.kt
+++ utils/src/main/kotlin/com/jacobtread/kme/utils/logging/LogWriter.kt
@@ -1,23 +1,30 @@
 package com.jacobtread.kme.utils.logging
 
 import java.io.IOException
-import java.io.RandomAccessFile
 import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
 import java.time.format.DateTimeFormatter
 import java.util.zip.GZIPOutputStream
 import kotlin.io.path.*
 
+/**
+ * LogWriter Handles writing of logs to disk as well as keeping
+ * a buffer for handling data < 4M before needing to write to disk
+ *
+ * @constructor Create empty LogWriter
+ */
 class LogWriter {
     private val file = Path("logs", "latest.log")
-    private val fileAccess: RandomAccessFile
+    private val fileChannel: FileChannel
     private val outputBuffer: ByteBuffer = ByteBuffer.allocate(4024)
 
     init {
         try {
             archiveOldLog()
-            fileAccess = createFileAccess()
+            fileChannel = createFileChannel()
+            // Shutdown hook for closing the file
             Runtime.getRuntime().addShutdownHook(Thread(this::close))
         } catch (e: IOException) {
             System.err.println("Failed to open RandomAccessFile to the logging file")
@@ -25,40 +32,62 @@ class LogWriter {
         }
     }
 
+    /**
+     * write Writes text to this log writer. If the text is small
+     * enough to fit into the output buffer then it is written there
+     * otherwise the output buffer is flushed at the text is written
+     * directly to the file channel
+     *
+     * @param text The text to write
+     */
     fun write(text: String) {
         val bytes = text.toByteArray()
         if (bytes.size > outputBuffer.remaining()) {
-            synchronized(fileAccess) {
+            synchronized(fileChannel) {
                 flush()
-                fileAccess.write(bytes)
+                fileChannel.write(ByteBuffer.wrap(bytes))
             }
         } else {
             outputBuffer.put(bytes)
         }
     }
 
+    /**
+     * flush Flushes the current contents of the output buffer
+     * by writing them all to the file channel
+     */
     @Synchronized
     fun flush() {
         outputBuffer.flip()
         try {
-            fileAccess.channel.write(outputBuffer)
+            fileChannel.write(outputBuffer)
         } finally {
             outputBuffer.clear()
         }
     }
 
+    /**
+     * createFileChannel Creates a file channel for writing to
+     * the latest log file this also ensures the logging directory
+     * as well as the logging file exists before opening the channel
+     *
+     * @return The opened file channel
+     */
     @Synchronized
-    private fun createFileAccess(): RandomAccessFile {
+    private fun createFileChannel(): FileChannel {
         val parent = file.parent
         if (parent.notExists()) parent.createDirectories()
         if (file.notExists()) file.createFile()
-        val fileAccess = RandomAccessFile(file.toFile(), "rw")
-        val length = fileAccess.length()
-        fileAccess.seek(length)
-        return fileAccess
+        return FileChannel.open(file, StandardOpenOption.CREATE, StandardOpenOption.APPEND)
     }
 
 
+    /**
+     * archiveOldLog Archives the old "latest.log" file by renaming it
+     * to the date of last modified yyyy-MM-dd-i.log.gz (e.g 2022-04-06-1.log.gz)
+     * "i" will increase until a file with that name is not taken. The
+     * contents of the log are compressed using GZIP
+     */
     @Synchronized
     fun archiveOldLog() {
         if (!file.isRegularFile()) return
@@ -82,11 +111,16 @@ class LogWriter {
         file.deleteExisting()
     }
 
+    /**
+     * close Handles closing the log writer this flushes
+     * the buffer writing everything to the log file and
+     * closing the random access file
+     */
     @Synchronized
     fun close() {
         try {
             flush()
-            fileAccess.close()
+            fileChannel.close()
         } catch (e: Exception) {
             println("Failed to save log")
             e.printStackTrace()
