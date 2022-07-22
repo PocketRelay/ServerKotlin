f1d3a3b4088716fd436e2106370e819f95593898
Jacobtread <jacobtread@gmail.com>
2022-07-22 23:50:15 +1200

    IN_LOBBY_LONGTIME gamestate

--- app/src/main/kotlin/com/jacobtread/kme/data/GameStateAttr.kt
+++ app/src/main/kotlin/com/jacobtread/kme/data/GameStateAttr.kt
@@ -2,6 +2,7 @@ package com.jacobtread.kme.data
 
 enum class GameStateAttr(val value: String) {
     IN_LOBBY("IN_LOBBY"),
+    IN_LOBBY_LONGTIME("IN_LOBBY_LONGTIME"),
     IN_GAME_STARTING("IN_GAME_STARTING"),
     IN_GAME_MIDGAME("IN_GAME_MIDGAME"),
     IN_GAME_FINISHING("IN_GAME_FINISHING")
