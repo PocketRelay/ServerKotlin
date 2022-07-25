9e3971f32816e98e1f1cee1585837511824f4c11
Jacobtread <jacobtread@gmail.com>
2022-07-25 23:33:55 +1200

    Documented challenge reward IDS

--- app/src/main/kotlin/com/jacobtread/kme/database/tables/PlayersTable.kt
+++ app/src/main/kotlin/com/jacobtread/kme/database/tables/PlayersTable.kt
@@ -25,8 +25,41 @@ object PlayersTable : IntIdTable("players") {
     /**
      * Challenge reward banner
      *
-     * 100: MEMstr.dds
+     * General:
      *
+     * - 100: Mass Effect   MEMstr.dds
+     * - 104: Squad Elite
+     * - 105: Spectre Master
+     * - 106: Solo Mastery
+     *
+     * - 125: N7 Mastery
+     * - 126: Map Mastery
+     * - 127: Biotic Mastery
+     * - 128: Tech Mastery
+     *
+     * Aliens:
+     * - 112: Resurgence Mastery
+     * - 113: Rebellion Mastery
+     * - 114: Earth Mastery
+     * - 115: Retaliation Mastery
+     * - 121: Blood Pack Mastery
+     * - 122: Commando Mastery
+     * - 123: Machine Mastery
+     * - 124: Outsider Mastery
+     * - 206: Reckoning Mastery
+     *
+     * Weapons:
+     *
+     * - 107: Shotgun Mastery
+     * - 108: Assault Rifle Mastery
+     * - 109: Pistol Mastery
+     * - 110: SMG Mastery
+     * - 111: Sniper Rifle Mastery
+     * - 116: Combat Mastery
+     * - 117: Ceberus Mastery
+     * - 118: Reaper Mastery
+     * - 119: Geth Mastery
+     * - 120: Collector Mastery
      */
     val challengeReward = integer("csreward")
         .default(0)
