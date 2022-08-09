a131184ddc8f59f7183e4b133863463c230bd7f4
Jacobtread <jacobtread@gmail.com>
2022-08-09 22:30:28 +1200

    Inlined hashing constants

--- src/main/kotlin/com/jacobtread/kme/utils/Password.kt
+++ src/main/kotlin/com/jacobtread/kme/utils/Password.kt
@@ -6,20 +6,6 @@ import java.util.regex.Pattern
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
-// The cost of the hashing process 1 < HASH_COST = total iterations
-private const val HASH_COST = 16
-
-// The length of the hash key
-private const val HASH_SIZE = 128
-
-// The hashing algorithm to use
-private const val HASH_ALGORITHM = "PBKDF2WithHmacSHA512"
-
-// The prefix to append to the hashed password
-private const val HASH_PREFIX = "$31$"
-
-private val HASH_PATTERN = Pattern.compile("\\$31\\$(\\d\\d?)\\$(.{43})")
-
 /**
  * hashPassword Hashes the password using the provided
  * HASH_ALGORITHM
@@ -28,20 +14,23 @@ private val HASH_PATTERN = Pattern.compile("\\$31\\$(\\d\\d?)\\$(.{43})")
  * @return The hashed password
  */
 fun hashPassword(password: String): String {
+    // The cost of the hashing process 1 < HASH_COST = total iterations
+    val hashCost = 16
     val random = SecureRandom()
     val salt = ByteArray(16)
     random.nextBytes(salt)
-    val hash = hashValue(password.toCharArray(), salt, HASH_COST)
+    val hash = hashValue(password.toCharArray(), salt, hashCost)
     val hashSalt = salt + hash
     val encoded = Base64.getUrlEncoder()
         .withoutPadding()
         .encodeToString(hashSalt);
-    return "$HASH_PREFIX$HASH_COST\$$encoded"
+    val prefix = "$31$"
+    return "$prefix$hashCost\$$encoded"
 }
 
 private fun hashValue(password: CharArray, salt: ByteArray, cost: Int): ByteArray {
-    val spec = PBEKeySpec(password, salt, 1 shl cost, HASH_SIZE)
-    val keyFactory = SecretKeyFactory.getInstance(HASH_ALGORITHM)
+    val spec = PBEKeySpec(password, salt, 1 shl cost, 128)
+    val keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
     return keyFactory.generateSecret(spec).encoded
 }
 
@@ -54,9 +43,10 @@ private fun hashValue(password: CharArray, salt: ByteArray, cost: Int): ByteArra
  * @return Whether the passwords match
  */
 fun comparePasswordHash(password: String, hashed: String): Boolean {
-    val hashMatcher = HASH_PATTERN.matcher(hashed)
+    val hashPattern = Pattern.compile("\\$31\\$(\\d\\d?)\\$(.{43})")
+    val hashMatcher = hashPattern.matcher(hashed)
     if (!hashMatcher.matches()) return false
-    val cost = hashMatcher.group(1).toIntOrNull() ?: HASH_COST
+    val cost = hashMatcher.group(1).toIntOrNull() ?: 16
     val contents = Base64.getUrlDecoder().decode(hashMatcher.group(2))
     val salt = contents.copyOfRange(0, 16)
     val hash = contents.copyOfRange(16, contents.size)
