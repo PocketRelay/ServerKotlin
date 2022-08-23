2e971784102ac5a49f9efb48159dc687d9229158
Jacobtread <jacobtread@gmail.com>
2022-08-23 19:55:42 +1200

    Removed unused exceptions

--- src/main/kotlin/com/jacobtread/relay/exceptions/CommandException.kt
+++
@@ -1,7 +0,0 @@
-package com.jacobtread.relay.exceptions
-
-class CommandException : Exception {
-    constructor(message: String) : super(message)
-    constructor(message: String, throwable: Throwable) : super(message, throwable)
-    constructor(throwable: Throwable) : super(throwable)
-}
